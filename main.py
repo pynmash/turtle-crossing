6ba09080fcbab0474a8cd70e3313e6be8309db6a
pynmash <0garrow@protonmail.com>
2024-09-13 22:45:22 +0100

    Add use of player class to add player character and manage forward movement

--- main.py
+++ main.py
@@ -5,10 +5,19 @@ from car_manager import CarManager
 from scoreboard import Scoreboard
 
 screen = Screen()
-screen.setup(width=600, height=600)
+screen.setup(width=600, height=600, startx=2000)
 screen.tracer(0)
+
+player = Player()
+
+screen.listen()
+
+screen.onkey(player.move, "Up")
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+
+screen.exitonclick()
